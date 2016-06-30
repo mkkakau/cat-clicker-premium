589444ea235be222d1039714382de36a3b0557a4
Melanie A <mkkakau@gmail.com>
2016-06-30 10:29:25 -1000

    feat: Add cat levels based on click count

--- js/app.js
+++ js/app.js
@@ -3,6 +3,18 @@ function CatClickerViewModel() {
   this.imgSrc = ko.observable('images/Box Cat.jpg');
   this.clickCount = ko.observable(0);
 
+  this.level = ko.computed(function () {
+    var clicks = this.clickCount();
+    if (clicks < 10) {
+      return 'Newborn';
+    }
+    else if (clicks >= 10 && clicks < 50) {
+      return 'Infant';
+    }
+    else {
+      return 'Teen';
+    }
+  }, this);
   this.incrementCounter = function () {
     this.clickCount(this.clickCount() + 1);
   };
